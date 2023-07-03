23158d5b80677496d5566534b4f27283289225a8
bi-pixel <zminig.1@gmail.com>
2023-07-03 22:50:06 +0300

    task_4_4 complete

--- exercises/04_data_structures/task_4_4.py
+++ exercises/04_data_structures/task_4_4.py
@@ -24,3 +24,12 @@
 """
 
 vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+
+vlans.sort()
+result = []
+
+for vlan in vlans:
+    if result.count(vlan) == 0:
+        result.append(vlan)
+        
+print(result)
