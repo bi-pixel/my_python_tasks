b5e34f0aa500e9058e40334cb34ecb914f5ea0d8
bi-pixel <zminig.1@gmail.com>
2023-07-07 14:22:42 +0300

    task_5_1 complete

--- exercises/05_basic_scripts/task_5_1.py
+++ exercises/05_basic_scripts/task_5_1.py
@@ -28,3 +28,5 @@ words = [
     'Guido', 'van', 'Rossum', 'began', 'working', 'on',
     'Python', 'in', 'the', 'late', '1980s',
 ]
+user_input = input('Enter separator: ')
+print(words[int(user_input)].lower())
