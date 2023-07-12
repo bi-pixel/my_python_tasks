71daa5256894b3120a3309f83617741225c4fb39
bi-pixel <zminig.1@gmail.com>
2023-07-12 10:10:24 +0300

    task_6_1 complete

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -17,3 +17,9 @@ $ python task_6_1.py
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+result = []
+
+for item in mac:
+    result.append(item.replace(':', '.'))
+
+print(result)
