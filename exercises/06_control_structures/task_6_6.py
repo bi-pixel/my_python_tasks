9ada9cd9f2a0715e635108d6272707c6c5677271
bi-pixel <zminig.1@gmail.com>
2023-07-13 16:51:47 +0300

    task_6_6 complete

--- exercises/06_control_structures/task_6_6.py
+++ exercises/06_control_structures/task_6_6.py
@@ -32,3 +32,17 @@ Enter IP address: 250.1.1.1
 unused
 
 """
+
+ip = input('Enter IP address: ')
+oct1 = int(ip.split('.')[0])
+
+if ip == '255.255.255.255':
+    print('local broadcast')
+elif ip == '0.0.0.0':
+    print('unassigned')
+elif 1 <= oct1 <= 223:
+    print('unicast')
+elif 224 <= oct1 <= 239:
+    print('multicast')
+else:
+    print('unused')
