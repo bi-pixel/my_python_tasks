fe20b0f53ad0d3e16eefcbd2ab5f9ab247ec5e92
bi-pixel <zminig.1@gmail.com>
2023-08-06 19:36:25 +0300

    task_7_2b complete

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -15,4 +15,20 @@ $ python task_7_2b.py config_sw1.txt new_config.txt
 ignore та рядки, що починаються на '!'.
 """
 
+from sys import argv
+
 ignore = ["duplex", "alias", "configuration", "end", "service"]
+
+if len(argv) > 2:
+    src_file, dst_file = argv[1], argv[2]
+else:
+    print('скрипт має містити 2 аргументи: старий та новий конфіг')
+    exit()
+
+config = ''
+with open(src_file, 'r') as src, open(dst_file, 'w') as dst:
+    for line in src:
+        if not any(elem in line for elem in ignore):
+            if not line.startswith('!'):
+                config += (line.rstrip() + '\n')
+    dst.write(config)
